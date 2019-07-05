e7bda5412febb288edf89e1c7d916a090fbcf239
eugenemunemo <j.babybehindmusic@gmail.com>
2019-07-05 05:47:35 -0700

    changing comments

--- js/temperature.js
+++ js/temperature.js
@@ -5,19 +5,19 @@ function myFunction(){
 		
 	//assign values
 	
-	if (temperature >=27) {
-		alert( "summer dresscode shorts sunglasses t-shirts");
-	   }
-	   else if (temperature <=26 && temperature >20){
-		alert(" wear long sleeve shirts trousers");
-	   }
-	   else if (temperature <=19 && temperature>=15){
-		alert(" wear heavy jackets woolen hats closed shoes");
-	   }
-	   else if (temperature <=14 && temperature >=-10){
-		alert(" wear long sleeve shirts, trousers");
-	   } 
-	   else {
-		alert("not a valid weather temparature");	
-	   }	
-	}
+	//if (temperature >=27) {
+	//	alert( "summer dresscode shorts sunglasses t-shirts");
+	 //  }
+	 //  else if (temperature <=26 && temperature >20){
+	//	alert(" wear long sleeve shirts trousers");
+	 //  }
+	  // else if (temperature <=19 && temperature>=15){
+	//	alert(" wear heavy jackets woolen hats closed shoes");
+	 //  }
+	  // else if (temperature <=14 && temperature >=-10){
+	//	alert(" wear long sleeve shirts, trousers");
+	 //  } 
+	 //  else {
+	//	alert("not a valid weather temparature");	
+	 //  }	
+//}
